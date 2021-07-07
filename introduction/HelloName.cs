1b975d7a9913b4d6c5d73aae512db0212ee0e9a3
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-07-07 16:07:11 +0900

    2021-07-07-1600

---
+++ introduction/HelloName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class HelloName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("あなたの名前は？");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}さん", name);
+        }//Main()
+    }//class
+}
+
+/*
+// 実行結果
+あなたの名前は？
+shika
+Hello, shikaさん
+続行するには何かキーを押してください . . .
+ */
