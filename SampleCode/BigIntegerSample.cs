af4309f1e7c215c2787ffe75c805df59befd6c81
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-08-12 11:52:56 +0900

    2021-08-12-1200

---
+++ SampleCode/BigIntegerSample.cs
@@ -0,0 +1,98 @@
+﻿/**
+ * @title CsharpBegin / SampleCode / GotoSample.cs
+ * @reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ * @content 第５章 標準ライブラリ / p203 /  5-49
+ *          BigIntegerクラス 階乗の表示
+ * @prepare ◆アセンブリの追加 System.Numerics.dll
+ *          [VS] ソリューションエクスプローラ -> プロジェクト右クリック
+ *          -> [追加] -> [参照]
+ *          -> 参照マネージャーダイアログが開く
+ *          -> 左ペイン -> [アセンブリ] -> [フレームワーク]
+ *          -> [System.Numerics] -> チェック -> [OK]
+ *          
+ * @author shika
+ * @date 2021-08-12
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.SampleCode
+{
+    class BigIntegerSample
+    {
+        static void Main(string[] args)
+        //internal void Main(string[] args)
+        {
+            //---- long / BigInteger 切替 ----
+            //long result = 1;
+            BigInteger result = 1;
+
+            //1～25の階乗を計算し出力
+            for(var i = 1; i < 26; i++)
+            {
+                result *= i;
+                Console.WriteLine(result);
+            }
+        }//Main()
+    }//class
+}
+
+/*
+//---- long ----
+1
+2
+6
+24
+120
+720
+5040
+40320
+362880
+3628800
+39916800
+479001600
+6227020800
+87178291200
+1307674368000
+20922789888000
+355687428096000
+6402373705728000
+121645100408832000
+2432902008176640000
+-4249290049419214848
+-1250660718674968576
+8128291617894825984
+-7835185981329244160
+7034535277573963776
+
+//---- BigInteger ----
+1
+2
+6
+24
+120
+720
+5040
+40320
+362880
+3628800
+39916800
+479001600
+6227020800
+87178291200
+1307674368000
+20922789888000
+355687428096000
+6402373705728000
+121645100408832000
+2432902008176640000
+51090942171709440000
+1124000727777607680000
+25852016738884976640000
+620448401733239439360000
+15511210043330985984000000
+ */
---
+++ SampleCode/MathSample.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * @title CsharpBegin / SampleCode / GotoSample.cs
+ * @reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ * @content 第５章 標準ライブラリ / p202 /  5-48
+ *          Mathクラスの staticメソッド
+ *          
+ * @author shika
+ * @date 2021-08-12
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.SampleCode
+{
+    class MathSample
+    {
+        //static void Main(string[] args)
+        internal void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-100));
+            Console.WriteLine(Math.Max(6, 3));
+            Console.WriteLine(Math.Min(6, 3));
+            Console.WriteLine(Math.Ceiling(1234.56));
+            Console.WriteLine(Math.Floor(1234.56));
+            Console.WriteLine(Math.Round(1234.56, MidpointRounding.AwayFromZero));
+
+            Console.WriteLine(Math.Sqrt(10000));
+            Console.WriteLine(Math.Pow(2, 4));
+            Console.WriteLine(Math.Sign(-100));
+
+            Console.WriteLine(Math.Cos(Math.PI / 180 * 60));
+            Console.WriteLine(Math.Sin(Math.PI / 180 * 30));
+            Console.WriteLine(Math.Tan(Math.PI / 180 * 45));
+
+            Console.WriteLine(Math.Log(125, 5));
+            Console.WriteLine(Math.Log10(100));
+            Console.WriteLine(Math.Exp(10));
+
+        }//Main()
+    }//class
+}
+
+/*
+100
+6
+3
+1235
+1234
+1235
+100
+16
+-1
+0.5
+0.5
+1
+3
+2
+
+ */
