aa8b4691c1c55cc51136bb0126f782962c9eac97
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-10-06 05:49:53 +0900

    2021-10-06-0600

---
+++ Utility/FileDocumentDiv/FileDocument.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.Utility.FileDocumentDiv
+{
+    class FileDocument
+    {
+        private ReferenceUtil reference = new ReferenceUtil();
+        private DateTime now = DateTime.Now;
+        private string dir;
+        private string document;
+
+        //public FileDocument()
+        //{
+        //    this.dir = SeekDir();
+        //}
+
+        public FileDocument(string dir)
+        {
+            this.dir = dir;
+            this.document = BuildDocument();
+        }
+
+        //public string SeekDir()
+        //{
+        //    StackTrace trace = new StackTrace();
+        //    StackFrame frame = trace.GetFrame(0);
+            
+        //    Console.WriteLine(frame.GetFileName());
+        //    return frame.GetFileName();
+        //}//SeekDir()
+
+        private string BuildDocument()
+        {
+            if(document == null)
+            {
+                string book = reference.SeekBook(this.dir);
+                string dirDoc = this.dir.Replace(@"\", " / ");
+
+                var bld = new StringBuilder(300);
+                bld.Append(@"/**").Append("\n");
+                bld.Append($" *@title {dirDoc} \n");
+                bld.Append($" *@reference {book} \n");
+                bld.Append( " *@content \n");
+                bld.Append( " * \n");
+                bld.Append( " *@author shika \n");
+                bld.Append($" *@date {now.ToString("yyyy-MM-dd")} \n");
+                bld.Append(@" */").Append("\n");
+
+                //Console.WriteLine($"bld.Length: {bld.Length}");
+                document = bld.ToString();
+            }
+
+            return document;
+        }//BuildDocument();
+
+        //====== Test Main() ======
+        static void Main(string[] args)
+        //public void Main(string[] args)
+        {
+            string dir = @"CsharpBegin\Utility\FileDocumentDiv\FileDocument.cs";
+            var here = new FileDocument(dir);
+
+            //---- Test BuildDocument() ----
+            Console.WriteLine(here.document);
+        }//Main()
+        
+    }//class
+}
+
+/*
+//---- Test BuildDocument() ----
+/＊＊
+ *@title CsharpBegin / Utility / FileDocumentDiv / FileDocument.cs
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@content
+ *
+ *@author shika
+ *@date 2021-10-06
+＊/
+bld.Length: 171
+
+*/
---
+++ Utility/ReferenceUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.Utility
+{
+    class ReferenceUtil
+    {
+        private Dictionary<string, string> ReferenceDic { get; } = 
+            new Dictionary<string, string>()
+            {
+                ["CsharpBegin"] =
+                "山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017",
+                ["Python"] = 
+                "山田祥寛『独習 Python』 翔泳社, 2020"
+            };
+        
+        public string SeekBook(string dir)
+        {            
+            string book = "";
+            foreach(string key in ReferenceDic.Keys)
+            {
+                if (dir.Contains(key))
+                {
+                    bool doneSeek = ReferenceDic.TryGetValue(key, out book);
+                    //Console.WriteLine($"doneSeek: {doneSeek}");
+                }
+            }//foreach key
+
+            return book;
+        }//SeekBook()
+
+        ////====== Test Main ======
+        //static void Main(string[] args)
+        //{
+        //    var here = new ReferenceUtil();
+        //    string dir = @"C:\Users\sophia\source\repos\CsharpBegin\CsharpBegin\Utility\ReferenceUtil.cs";
+        //    Console.WriteLine($"Directory: {dir}");
+        //    Console.WriteLine($"Reference: {here.SeekBook(dir)}");
+        //}//Main()
+    }//class
+}
+
+/*
+//---- Test Main() ----
+Directory: C:\Users\sophia\source\repos\CsharpBegin\CsharpBegin\Utility\ReferenceUtil.cs
+doneSeek: True
+Reference: 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+
+ */
