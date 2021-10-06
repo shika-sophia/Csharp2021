6c2711dedc338a350c184b03de081d8777e01e9a
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-10-07 08:16:17 +0900

    2021-10-07-0800

--- Utility/FileDocumentDiv/FileDocument.cs
+++ Utility/FileDocumentDiv/FileDocument.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/**
+ *@title CsharpBegin / Utility / FileDocumentDiv / FileDocument.cs
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@content ファイル先頭の documentation commentを作成するクラス
+ *@class FileDocument
+ *       / ◇ReferenceUtil referance,
+ *         - string absDir, //絶対パス
+ *         - string relDir, //projectからの相対パス
+ *         - string content,
+ *         + string document /
+ *       + new FileDocument(string) 
+ *       - string SeekDir() 
+ *           //static Main()を実行した fileName(絶対パス)を抽出。
+ *       - string BuildDocument()
+ *           //documentを作成     
+ *         
+ *@class ReferenceUtil
+ *       / - Dictionary<string,string> fileDic /
+ *       + List<string> SeekBook(string dir)
+ *       
+ *@author shika       
+ *@date 2021-10-07
+ */
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -11,35 +34,44 @@ namespace CsharpBegin.Utility.FileDocumentDiv
     {
         private ReferenceUtil reference = new ReferenceUtil();
         private DateTime now = DateTime.Now;
-        private string dir;
-        private string document;
+        private string absDir; //絶対パス
+        private string relDir; //projectからの相対パス
+        private string content;
+        public string document; 
 
-        //public FileDocument()
-        //{
-        //    this.dir = SeekDir();
-        //}
+        public FileDocument() : this("") { }
 
-        public FileDocument(string dir)
+        public FileDocument(string content)
         {
-            this.dir = dir;
+            this.absDir = SeekDir();
+            this.relDir = absDir.Substring(
+                absDir.LastIndexOf(@"CsharpBegin\"));            
+            this.content = content;            
             this.document = BuildDocument();
         }
 
-        //public string SeekDir()
-        //{
-        //    StackTrace trace = new StackTrace();
-        //    StackFrame frame = trace.GetFrame(0);
-            
-        //    Console.WriteLine(frame.GetFileName());
-        //    return frame.GetFileName();
-        //}//SeekDir()
+        public string SeekDir()
+        {
+            StackTrace trace = new StackTrace(true);
+            string fileName = null;
+            for(int i = 0; i < trace.FrameCount; i++)
+            {
+                StackFrame frame = trace.GetFrame(i);
+                fileName = frame.GetFileName();
+
+                if(String.IsNullOrEmpty(fileName)){ break; }
+            }//for
+
+            absDir = fileName;
+            return fileName;
+        }//SeekDir()
 
         private string BuildDocument()
         {
             if(document == null)
             {
-                List<string> bookList = reference.SeekBook(dir);
-                string dirDoc = dir.Replace(@"\", " / ");
+                List<string> bookList = reference.SeekBook(relDir);
+                string dirDoc = relDir.Replace(@"\", " / ");
 
                 var bld = new StringBuilder(300);
                 bld.Append(@"/**").Append("\n");
@@ -48,7 +80,7 @@ namespace CsharpBegin.Utility.FileDocumentDiv
                 {
                     bld.Append($" *@reference {book} \n");
                 }
-                bld.Append( " *@content \n");
+                bld.Append($" *@content {content} \n");
                 bld.Append( " * \n");
                 bld.Append( " *@author shika \n");
                 bld.Append($" *@date {now.ToString("yyyy-MM-dd")} \n");
@@ -64,11 +96,13 @@ namespace CsharpBegin.Utility.FileDocumentDiv
         ////====== Test Main() ======
         //static void Main(string[] args)
         ////public void Main(string[] args)
-        //{
-        //    string dir = @"CsharpBegin\Utility\Python\FileDocument.cs";
-        //    var here = new FileDocument(dir);
+        //{ 
+        //    var here = new FileDocument("第７章 オブジェクト指向基本");
 
-        //    //---- Test BuildDocument() ----
+        //    //---- Test constructor ----
+        //    Console.WriteLine($"absDir: {here.absDir}");
+        //    Console.WriteLine($"relDir: {here.relDir}");
+        //    Console.WriteLine($"content: {here.content}");
         //    Console.WriteLine(here.document);
         //}//Main()
 
@@ -78,14 +112,17 @@ namespace CsharpBegin.Utility.FileDocumentDiv
 /*
 //---- Test BuildDocument() ----
 /＊＊
- *@title CsharpBegin / Utility / Python / FileDocument.cs
+ *@title CsharpBegin / Utility / FileDocumentDiv / FileDocument.cs
  *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
- *@reference 山田祥寛『独習 Python』 翔泳社, 2020 
- *@content
+ *@content 第７章 オブジェクト指向基本
  *
  *@author shika
- *@date 2021-10-06
+ *@date 2021-10-07
 ＊/
 bld.Length: 202
 
+absDir: C:\Users\sophia\source\repos\CsharpBegin
+\CsharpBegin\Utility\FileDocumentDiv\FileDocument.cs
+relDir: CsharpBegin\Utility\FileDocumentDiv\FileDocument.cs
+content: 第７章 オブジェクト指向基本
 */
---
+++ Utility/FileDocumentDiv/InsertDocument.cs
@@ -0,0 +1,32 @@
+﻿/**
+ *@title CsharpBegin / Utility / FileDocumentDiv / InsertDocument.cs
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@content documentを Main()実行ファイルに挿入するクラス
+ *     そのファイルの既述のコードを事前に読み取り、
+ *     すでに documentがあれば、処理なし。
+ *     documentが未挿入なら、ファイル先頭に挿入し、
+ *     既述のコードを Appendする。
+ *
+ *@author shika
+ *@date 2021-10-07
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.Utility.FileDocumentDiv
+{
+    class InsertDocument
+    {
+        static void Main(string[] args)
+        //public void Main(string[] args)
+        {
+            var fileDoc = new FileDocument();
+            string document = fileDoc.document;
+            Console.WriteLine(document);
+        }//Main()
+
+    }//class
+}
