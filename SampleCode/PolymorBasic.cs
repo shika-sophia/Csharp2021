dc941ea3cdabbfb151a9d7b61711867313ed0f46
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-10-16 14:10:30 +0900

    2021-10-16-1400

---
+++ SampleCode/PolymorBasic.cs
@@ -0,0 +1,85 @@
+/**
+ *@title CsharpBegin / SampleCode / PolymorBasic.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content 第８章 Polymorphism ポリモーフィズム / p366 / List 8-25, 8-26
+ *    ◆abstract
+ *    ◆異なる継承オブジェクトを変数型で同一視
+ *@class PolymorBasic / ◆Main / new Triangle(), new Rectangle()
+ *@class AbsFigure / abstract
+ *       / +double Width, +double Height /
+ *       + AbsFigure(double width, double height)
+ *       + GetArea() abstract
+ *@class Triangle : AbsFigure
+ *       + TriAngle(double, double) : base(double, double)
+ *       + GetAngle() override
+ *@class Rectangle : AbsFigure
+ *       + RectAngle(double, double) : base(double, double)
+ *       + GetAngle() override
+ *
+ *@see PolymorNewHidden.cs
+ *@author shika 
+ *@date 2021-10-16 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.SampleCode
+{
+    class PolymorBasic
+    {
+        //static void Main(string[] args)
+        public void Main(string[] args)
+        {
+            AbsFigure figue = new Triangle(10, 30);
+            Console.WriteLine($"Triangle: {figue.GetArea()}");
+
+            figue = new Rectangle(10, 30);
+            Console.WriteLine($"Rectangle: {figue.GetArea()}");
+        }//Main()
+    }//class
+
+    abstract class AbsFigure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public AbsFigure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public abstract double GetArea();
+
+    }//abstract class
+
+    class Triangle : AbsFigure
+    {
+        public Triangle(double width, double height)
+            : base(width, height) { }
+        
+        public override double GetArea()
+        {
+            return Width * Height / 2;
+        }
+    }//class Triangle
+
+    class Rectangle : AbsFigure
+    {
+        public Rectangle(double width, double height)
+            : base(width, height) { }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }//class Rectangle
+}
+
+/*
+Triangle: 150
+Rectangle: 300
+ */
---
+++ SampleCode/PolymorNewHidden.cs
@@ -0,0 +1,76 @@
+﻿/**
+ *@title CsharpBegin / SampleCode / PolymorNewHidden.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content 第８章 Polymorphism ポリモーフィズム / p369 / List 8-A
+ *    ◆new修飾子によるメソッドの隠蔽だとポリモーフィズムが働かない例
+ *
+ *@see PolymorBasic.cs
+ *@author shika 
+ *@date 2021-10-16 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.SampleCode
+{
+    class PolymorNewHidden
+    {
+        //static void Main(string[] args)
+        public void Main(string[] args)
+        {
+            FigureHidden figue = new TriangleHidden(10, 30);
+            Console.WriteLine($"Triangle: {figue.GetArea():F}");
+
+            figue = new RectangleHidden(10, 30);
+            Console.WriteLine($"Rectangle: {figue.GetArea():F}");
+        }//Main()
+    }//class
+
+    class FigureHidden
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public FigureHidden(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double GetArea()
+        {
+            return 0.0d;
+        }
+
+    }//class FigureHidden
+
+    class TriangleHidden : FigureHidden
+    {
+        public TriangleHidden(double width, double height)
+            : base(width, height) { }
+
+        public new double GetArea()
+        {
+            return Width * Height / 2;
+        }
+    }//class TriangleHidden
+
+    class RectangleHidden : FigureHidden
+    {
+        public RectangleHidden(double width, double height)
+            : base(width, height) { }
+
+        public new double GetArea()
+        {
+            return Width * Height;
+        }
+    }//class Rectangle
+}
+
+/*
+Triangle: 0.00
+Rectangle: 0.00
+ */
