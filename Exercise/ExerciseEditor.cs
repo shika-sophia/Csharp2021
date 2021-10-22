fc93924aec905e09fffae5fd469171942b33ae62
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-10-22 17:47:45 +0900

    2021-10-22-1800

--- Exercise/ExerciseEditor.cs
+++ Exercise/ExerciseEditor.cs
@@ -1,4 +1,12 @@
-/**
+/* ====== Template ======
+static void Main(string[] args)
+//public void Main(string[] args)
+{
+    new CsharpBegin.Exercise.ExerciseEditor("");
+}//Main() 
+
+*/
+/** ====== Document ======
  *@title CsharpBegin / Exercise / ExerciseEditor.cs 
  *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
  *@content 練習問題用のアプリ / Documentの作成 / Appendixに日時と正答率
@@ -19,7 +27,7 @@ namespace CsharpBegin.Exercise
 {
     class ExerciseEditor
     {
-        private readonly InsertDocument doc;
+        private readonly FileDocExecute fileExe;
         private readonly MultiScan multi;
         private readonly CorrectScan correct;
 
@@ -27,7 +35,7 @@ namespace CsharpBegin.Exercise
         
         public ExerciseEditor(string content)
         {
-            this.doc = new InsertDocument(content);
+            this.fileExe = new FileDocExecute();
             this.multi = new MultiScan();
             this.correct = new CorrectScan(multi);
             ExerciseExe();
@@ -44,11 +52,12 @@ namespace CsharpBegin.Exercise
 
             //---- build result ----
             string date = start.ToString("yyyy-MM-dd(ddd)");
-            string contentAll = multi.ShowList(
+            string result = multi.ShowList(
                 correct.correctAnsList, $"==== Exercise Result / {date} ====");
             string appendix = BuildAppendix(date, start, finish);
 
-            //---- File Write ----
+            //---- File ReadWrite ----
+            fileExe.ReadWriteExe(document: "", appendix, result);
         }//ExerciseExe()
 
         private string BuildAppendix(
@@ -64,7 +73,7 @@ namespace CsharpBegin.Exercise
                 bld.Append($"{finish.ToString("HH:mm")} ");
                 bld.Append($"({costTime}分) \n");
             bld.Append($" *@rate: {correct.CalcCorrectRate()} \n");
-            bld.Append(" */ \n");
+            bld.Append("*/ \n");
 
             //Console.WriteLine($"bld.Length: {bld.Length}");
             Console.WriteLine(bld.ToString());
@@ -74,27 +83,85 @@ namespace CsharpBegin.Exercise
         //static void Main(string[] args)
         public void Main(string[] args)
         {
-            new ExerciseEditor();
-            //new Utility.FileDocumentDiv.InsertDocument("").InsertExe();
+            new ExerciseEditor("");
         }//Main()
     }//class
 }
 
-/* 
-==== Exercise Result / 2021-10-21(木) ====
+/*
+==== Question ====
+◆Quest:[-99: to Answer] [-88: Return]
+〔1〕Q1
+〔2〕Q2
+〔3〕Q3
+〔4〕-99
+<?> Quest(3)で終了します。
+終了 よろしいですか？ [Y / N]: t
+t は不正な入力です。
+[Y / N] で入力してください。
+
+終了 よろしいですか？ [Y / N]: y
+
+==== Answer ====
+◆〔1〕Q1
+◇Answer:[-99: QuestEnd] [-88: Return]
+(1) A1 - 1
+(2) A1 - 2
+(3) - 99
+<?> Answer(2)で終了します。
+終了 よろしいですか？ [Y / N]: y
+
+◆〔2〕Q2
+◇Answer:[-99: QuestEnd] [-88: Return]
+(1) A2 - 1
+(2) A2 - 2
+(3) - 99
+<?> Answer(2)で終了します。
+終了 よろしいですか？ [Y / N]: y
+
+◆〔3〕Q3
+◇Answer:[-99: QuestEnd] [-88: Return]
+(1) A3 - 1
+(2) - 99
+<?> Answer(1)で終了します。
+終了 よろしいですか？ [Y / N]: y
+
+==== Judge Correct ====
+◇正誤入力:[0: ○ ] [1: × ] [-88: Return]
+◆〔1〕Q1
+(1) A1 - 1: 0
+  (2) A1 - 2: 1
+ => ○: modi
+
+◆〔2〕Q2
+(1) A2 - 1: 0
+  (2) A2 - 2: 0
+
+◆〔3〕Q3
+(1) A3 - 1: 1
+ => ○: modi
+correctRate: 60.00％ (○ 3 問 / 全 5 問)
+bld.Length: 159
+/＊
+==== Exercise Result / 2021-10-22(金) ====
 ◆〔1〕Q1
 ○ (1) A1-1
 × (2) A1-2 => ○: modi
 
 ◆〔2〕Q2
-× (1) A2-1 => ○: modi
+○ (1) A2-1
 ○ (2) A2-2
-○ (3) A2-3
-*/
 
-//bld.Length: 104
-/*==== Appendix ====
- *@date: 2021-10-21(木)
- *@time: 14:48 ～ 14:48 (1分)
+◆〔3〕Q3
+× (1) A3-1 => ○: modi
+＊/
+
+/＊==== Appendix ====
+ *@date: 2021-10-22(金)
+ *@time: 17:37 ～ 17:39 (1分)
  *@rate: 60.00％ (○ 3 問 / 全 5 問)
- */
+＊/
+
+<○> this Document just has inserted.
+
+*/
---
+++ Exercise/ExerciseSample.cs
@@ -0,0 +1,43 @@
+/** 
+ *@title CsharpBegin / Exercise / ExerciseSample.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content 
+ * 
+ *@author shika 
+ *@date 2021-10-22 
+*/ 
+/*==== Appendix ==== 
+ *@date: 2021-10-22(金) 
+ *@time: 17:37 ～ 17:39 (1分) 
+ *@rate: 60.00％ (○ 3 問 / 全 5 問) 
+*/ 
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+ 
+namespace CsharpBegin.Exercise 
+{ 
+    class ExerciseSample 
+    { 
+        static void Main(string[] args) 
+        //public void Main(string[] args) 
+        { 
+            new CsharpBegin.Exercise.ExerciseEditor(""); 
+        }//Main()  
+    }//class 
+} 
+/* 
+==== Exercise Result / 2021-10-22(金) ==== 
+◆〔1〕Q1 
+○ (1) A1-1 
+× (2) A1-2 => ○: modi 
+
+◆〔2〕Q2 
+○ (1) A2-1 
+○ (2) A2-2 
+
+◆〔3〕Q3 
+× (1) A3-1 => ○: modi 
+*/ 
