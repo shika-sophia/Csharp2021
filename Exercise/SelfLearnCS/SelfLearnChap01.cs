f4831e690ee37da842c959c84e475664ba597954
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-10-23 11:33:32 +0900

    2021-10-23-1100

---
+++ Exercise/SelfLearnCS/SelfLearnChap01.cs
@@ -0,0 +1,59 @@
+/** 
+ *@title CsharpBegin / Exercise / SelfLearnCS / SelfLearnChap01.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content 第１章 章末問題
+ * 
+ *@author shika 
+ *@date 2021-10-23 
+*/ 
+/*==== Appendix ==== 
+ *@date: 2021-10-23(土) 
+ *@time: 10:37 ～ 10:46 (9分) 
+ *@rate: 91.67％ (○ 11 問 / 全 12 問) 
+*/ 
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+ 
+namespace CsharpBegin.Exercise.SelfLearnCS 
+{ 
+    class SelfLearnChap01 
+    { 
+        //static void Main(string[] args) 
+        public void Main(string[] args) 
+        { 
+            new CsharpBegin.Exercise.ExerciseEditor(""); 
+        }//Main()  
+    }//class 
+}
+/* 
+==== Exercise Result / 2021-10-23(土) ==== 
+◆〔1〕C#の特徴 
+× (1) マルチパラダイム: => ○: 後述 
+○ (2) マルチプラットフォーム: OSに関係なく動作すること。 
+
+◆〔2〕コード構成 
+○ (1) 名前空間 
+○ (2) クラス宣言 
+○ (3) メソッド宣言 
+○ (4) コメント 
+○ (5) 標準出力・入力 (コンソール入出力) 
+
+◆〔3〕開始メソッド 
+○ (1) static vois Main(string[] args) エントリーポイント 
+
+◆〔4〕文の末尾 
+○ (1) 「;」 
+
+◆〔5〕コメント記法 
+○ (1) 「//」一行コメント 
+○ (2) 「/＊ ＊/」複数行コメント 
+○ (3) 「///」ドキュメントコメント 
+*/
+/*==== Appendix ====
+ *@date: 2021-10-23(土)
+ *@time: 10:37 ～ 10:46 (9分)
+ *@rate: 91.67％ (○ 11 問 / 全 12 問)
+*/
