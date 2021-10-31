d7782db68bdd284bb8f0a7a1d385d31667e25b0d
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-10-31 17:01:49 +0900

    2021-10-31-1700

---
+++ SampleCode/OperatorEquals.cs
@@ -0,0 +1,117 @@
+/** 
+ *@title CsharpBegin / SampleCode / OperatorEquals.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content 9.6 OperatorEquals / p460 / List 9-54
+ *@subject public static operator ==(object, object) 「==」を再定義 
+ *         public static operator !=(object, object) 「!=」を再定義 
+ *
+ *@notice  (註)static method はコメントアウト中。
+ *         / static methods have been to comment out.
+ *         
+ *         「==」を再定義すると「!=」も再定義が必要
+ *         「==」を再定義すると、
+ *         Object.Equals(), Object.GetHashCode()の overrideも求められる。
+ *         
+ *         null判定の前に(object)キャストをしないと、
+ *         ここの「==」オーバーロードが呼び出され永久ループとなるので注意。
+ *
+ *@see EqualOverrideSample / PersonOverrideSample.cs
+ *@author shika 
+ *@date 2021-10-31 
+*/
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+ 
+namespace CsharpBegin.SampleCode 
+{ 
+    class OperatorEquals 
+    {
+        //static void Main(string[] args)
+        public void Main(string[] args) 
+        {
+            //==== Test OpeEquals ====
+            var p1 = new PersonOpe("Sophia", "Shika");
+            var p2 = p1;
+            var p3 = new PersonOpe("Sophia", "Shika");
+            var p4 = new PersonOpe("Lily", "Kouhara");
+
+            var personList = new List<PersonOpe> {p1, p2, p3, p4};
+            int count = 1;
+            personList.ForEach(p => Console.WriteLine($"p{count++} {p}"));
+
+            Console.WriteLine($"Reference / p1.Equals(p2): {p1.Equals(p2)}");
+            Console.WriteLine($"Property Value / p1 == p2: {p1 ==p2}");
+            Console.WriteLine($"Reference / p1.Equals(p3): {p1.Equals(p3)}");
+            Console.WriteLine($"Property Value / p1 == p3: {p1 == p3}");
+            Console.WriteLine($"Reference / p1.Equals(p3): {p1.Equals(p4)}");
+            Console.WriteLine($"Property Value / p1 == p4: {p1 == p4}");
+        }//Main() 
+    }//class 
+
+    class PersonOpe
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public PersonOpe(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        //==== 「==」Property同値性 ====
+        //public static bool operator ==(PersonOpe p1, PersonOpe p2)
+        //{
+        //    if(Object.ReferenceEquals(p1, p2)) 
+        //    { 
+        //        return true;
+        //    }
+        //    if((object) p1 == null || (object) p2 == null ||
+        //        p1.GetType() != p2.GetType())
+        //    {
+        //        return false;
+        //    }
+
+        //    return p1.FirstName == p2.FirstName && 
+        //           p1.LastName == p2.LastName;
+        //}//「==」
+
+        ////==== 「!=」: this.「==」Property同値性を反転 ====
+        //public static bool operator !=(PersonOpe p1, PersonOpe p2)
+        //{
+        //    return !(p1 == p2);
+        //}
+
+        //====  参照同一性 Object.Equals() ====
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() ^ LastName.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}";
+        }
+    }//class
+}
+
+/*
+p1 Name: Sophia Shika
+p2 Name: Sophia Shika
+p3 Name: Sophia Shika
+p4 Name: Lily Kouhara
+Reference / p1.Equals(p2): True
+Property Value / p1 == p2: True
+Reference / p1.Equals(p3): False
+Property Value / p1 == p3: True
+Reference / p1.Equals(p3): False
+Property Value / p1 == p4: False
+ */
---
+++ SampleCode/OperatorPlus.cs
@@ -0,0 +1,105 @@
+/**        
+ *@title CsharpBegin / SampleCode / OperatorPlus.cs        
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017        
+ *@content 9.6 OperatorPlus / p462 / List 9-55, 9-56, 9-57
+ *@subject OpePlus「+」
+ *@subject OpePlusProperty「obj + int」 
+ *@subject OpeIncrement「++」「--」
+ *         呼出時の「++」で、もとのオブジェクトもインクリメントされるので
+ *         毎回 新規オブジェクトをベースに「++」
+ *         前置と後置で結果が異なるので注意
+ *         
+ *@notice 【註】static methods have been to comment out.
+ *
+ *@subject OpeTrueFalse        
+ *@subject OpeCast        
+ *       
+ *@author shika        
+ *@date 2021-10-31        
+*/       
+using System;        
+using System.Collections.Generic;        
+using System.Linq;        
+using System.Text;        
+using System.Threading.Tasks;        
+        
+namespace CsharpBegin.SampleCode        
+{        
+    class OperatorPlus        
+    {        
+        //static void Main(string[] args)        
+        public void Main(string[] args)        
+        {
+            var co1 = new CoordinateOpe { X = 10, Y = 20 };
+            var co2 = new CoordinateOpe { X = 17, Y = 25 };
+            //var co3 = co1 + co2;
+            //var co4 = co1 + 100;
+
+            //var coBase = new CoordinateOpe { X = 10, Y = 20 };
+            //var co5 = ++coBase;
+            //coBase = new CoordinateOpe { X = 10, Y = 20 };
+            //var co6 = coBase++;
+
+            //var coList = new List<CoordinateOpe> { co1, co2, co3, co4, co5, co6 };
+            //int count = 1;
+            //coList.ForEach(co => Console.WriteLine($"co{count++}: {co}"));
+        }//Main()        
+    }//class
+    
+    class CoordinateOpe
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        ////CoordinateOpeオブジェクト同士の「+」を再定義
+        //public static CoordinateOpe operator + (CoordinateOpe co1, CoordinateOpe co2)
+        //{
+        //    return new CoordinateOpe()
+        //    {
+        //        X = co1.X + co2.X,
+        //        Y = co1.Y + co2.Y
+        //    };
+        //}//operator obj + obj
+
+        ////CoordinateOpe + intの「+」を再定義
+        //public static CoordinateOpe operator +(CoordinateOpe co1, int x)
+        //{
+        //    return new CoordinateOpe()
+        //    {
+        //        X = co1.X + x,
+        //        Y = co1.Y
+        //    };
+        //}//operator obj + int
+
+        //public static CoordinateOpe operator ++ (CoordinateOpe co)
+        //{
+        //    return new CoordinateOpe()
+        //    {
+        //        X = co.X + 1,
+        //        Y = co.Y + 1
+        //    };
+        //}//operator ++
+
+        //public static CoordinateOpe operator --(CoordinateOpe co)
+        //{
+        //    return new CoordinateOpe()
+        //    {
+        //        X = co.X - 1,
+        //        Y = co.Y - 1
+        //    };
+        //}//operator --
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y:{Y}";
+        }
+    }//class
+}
+/*
+co1: X: 10, Y:20
+co2: X: 17, Y:25
+co3: X: 27, Y:45
+co4: X: 110, Y:20
+co5: X: 11, Y:21
+co6: X: 10, Y:20
+*/
