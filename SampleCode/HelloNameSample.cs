ba92269757b786b552cf0441c4125c381e5c880b
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-11-18 06:36:39 +0900

    2021-11-18-0630

---
+++ SampleCode/HelloNameSample.cs
@@ -0,0 +1,38 @@
+/**
+ *@title CsharpBegin / SampleCode / HelloNameSample.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content 名前を聞いて、固定文字列に名前を入れて出力
+ *         string Console.ReadLine();      //コンソール入力
+ *         void Console.WriteLine(string); //コンソール出力
+ * 
+ *@author shika 
+ *@date 2021-08-07 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.SampleCode
+{
+    class HelloNameSample
+    {
+        public void Main(string[] args)
+        //static void Main(string[] args)
+        {
+            Console.WriteLine("あなたの名前は？");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}さん", name);
+        }//Main()
+
+    }//class
+}
+
+/*
+// 実行結果
+あなたの名前は？
+shika
+Hello, shikaさん
+続行するには何かキーを押してください . . .
+ */
