f93d9dbdc92c1ca7db7712ef240f0d50b1cc6380
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-11-18 06:38:59 +0900

    2021-11-18-0630

---
+++ Application/MainExecute.cs
@@ -0,0 +1,208 @@
+﻿/**
+ * @title CsharpBegin / A00_Template / MainExecute.cs
+ * @reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ * @content MainExecute
+ *   VS仕様で static Main()は１つのみなので、
+ *   各クラスは internal Main()とコメントアウトで切替して利用中。
+ *   
+ *   DirectoryInfoで プロジェクト内のファイルを検索し、
+ *   最新更新ファイルをインスタンス化。
+ *   internal Main()を呼び出して実行するプログラム。
+ *       
+ * @refernce ◆Microsoft Document / Type.GetMethod メソッド
+ *   https://docs.microsoft.com/ja-jp/dotnet/api/system.type.getmethod?view=net-5.0#System_Type_GetMethod_System_String_System_Int32_System_Reflection_BindingFlags_System_Reflection_Binder_System_Reflection_CallingConventions_System_Type___System_Reflection_ParameterModifier___
+ *   
+ *   public System.Reflection.MethodInfo
+ *   GetMethod (string name, [int genericParameterCount,]
+ *     System.Reflection.BindingFlags bindingAttr,
+ *     System.Reflection.Binder? binder,
+ *     [System.Reflection.CallingConventions callConvention,]
+ *     Type[] types,
+ *     System.Reflection.ParameterModifier[]? modifiers);
+ *   
+ * @author shika
+ * @date 2021-09-29
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.A00_Template
+{
+    class MainExecute
+    {
+        //static void Main(string[] args)
+        internal void Main(string[] args)
+        {
+            string dir = @"C:\Users\sophia\source\repos\CsharpBegin\CsharpBegin\";
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+            DirectoryInfo[] dirAry = dirInfo.GetDirectories("*", SearchOption.AllDirectories);
+            DateTime lastTime = new DateTime(1, 1, 1, 0, 0, 0);
+            FileInfo lastFile = null;
+            StringBuilder bld = new StringBuilder();
+            int fileCount = 0;
+
+            foreach(DirectoryInfo info in dirAry)
+            {
+                bld.Append("◆" + info + "\n");
+
+                if(info.Name.Equals("SampleCode") 
+                    || info.Name.Contains("Template")
+                    || info.Name.Equals("Utility")
+                    || info.Name.Equals("Application"))
+                {
+                    FileInfo[] fileAry = info.GetFiles();
+                    foreach (FileInfo file in fileAry)
+                    {
+                        bld.Append($"  └ [{fileCount}] {file} \n");
+                        fileCount++;
+
+                        DateTime tempLastTime = file.LastWriteTime;
+                        if (lastTime <= tempLastTime)
+                        {
+                            lastTime = tempLastTime;
+                            lastFile = file;
+                        }
+                    }//foreach fileAry
+                }//if
+            }//foreach dirAry
+
+            Console.WriteLine("lastFile: " + lastFile.FullName);
+            Console.WriteLine("lastFile: " + lastFile.Name);
+            Console.WriteLine();
+
+            if (lastFile.Name.Contains(nameof(MainExecute)))
+            {
+                Console.WriteLine(bld.ToString());
+                Console.WriteLine("実行できません。");
+                Console.WriteLine("実行するFileを選んでください。");
+            }
+            //本来は else句に配置
+            string lastDir = MakeLastDir(lastFile);
+            string useClass = lastDir + lastFile.Name.Replace(".cs", "");
+            Console.WriteLine($"useClass: {useClass}");
+
+            Type exeType = useClass.GetType();
+            Console.WriteLine($"exeType: {exeType}");
+
+            object instance = Activator.CreateInstance(exeType);
+            MethodInfo main = exeType.GetMethod(
+                "Main", new Type[] { typeof(string[]) });
+            main.Invoke(instance, new[] { args });
+        }//Main()
+
+        private static string MakeLastDir(FileInfo lastFile)
+        {
+            string lastDir = lastFile.DirectoryName;
+            lastDir = lastDir.Substring(lastDir.LastIndexOf("CsharpBegin"))
+                .Replace("\\",".") + ".";
+            
+            return lastDir;
+        }//MakeLastDir
+    }//class
+
+}
+
+/*
+◆//using System.Diagnostics;
+    StackTrace st = new StackTrace();
+    Console.WriteLine(st.ToString());
+    Console.WriteLine(st.GetType());
+
+    //Class.Main(args); //internal Main()を呼出
+＊st.ToString()
+場所 CsharpBegin.A00_Template.
+MainExecute.MainTemplate.Main(String[] args)
+
+＊st.GetType()
+System.Diagnostics.StackTrace
+
+//---- DirectoyInfo.GetDirectories() ----
+lastFile: C:\Users\sophia\source\repos\CsharpBegin\CsharpBegin\A00-Template\MainExecute.cs
+lastFile: MainExecute.cs
+
+◆A00-Template
+  └ [0] DateTimeTemplate.cs
+  └ [1] MainExecute.cs
+  └ [2] MainTemplate.cs
+  └ [3] RegexTemplate.cs
+◆bin
+◆Data
+◆obj
+◆Properties
+◆SampleCode
+  └ [4] BigIntegerSample.cs
+  └ [5] ComparerSample.cs
+  └ [6] ConcatString.cs
+  └ [7] ConstructorSample.cs
+  └ [8] DictionarySample.cs
+  └ [9] DisposeSample.cs
+  └ [10] FileDirectorySample.cs
+  └ [11] GoToSample.cs
+  └ [12] HelloName.cs
+  └ [13] LinkedListSample.cs
+  └ [14] ListSample.cs
+  └ [15] MathSample.cs
+  └ [16] MulitiplyMatrix.cs
+  └ [17] PreprocesserDirectiveSample.cs
+  └ [18] QueueSample.cs
+  └ [19] SetSample.cs
+  └ [20] SingletonSample.cs
+  └ [21] SortedDictionarySample.cs
+  └ [22] SortedSetSample.cs
+  └ [23] StackSample.cs
+  └ [24] StreamWriterReaderSample.cs
+  └ [25] StringFormatSample.cs
+  └ [26] StringSample.cs
+  └ [27] StringSearchSample.cs
+  └ [28] StringTreatSample.cs
+◆Debug
+◆Release
+◆Debug
+◆Release
+◆TempPE
+
+実行できません。
+実行するFileを選んでください。
+CsharpBegin.A00_Template.MainExecute
+
+【考察】exeClassで System.NullReferenceExceptionが発生。
+path違いで ClassNotFoundの可能性もある。
+=> GetType(namespace + fileName)で成功。絶対パスではなく、
+usingで使う クラスの完全修飾名なので、区切り文字は「.」
+    Type.GetType(
+        "CsharpBegin.A00_Template." +
+        lastFile.Name.Replace(".cs", ""));
+
+【考察】
+MethodInfo main = exeClass.GetMethod(
+    "Main", BindingFlags.NonPublic, null, 
+    new[] { typeof(string[]) }, new ParameterModifier[] { });
+Console.WriteLine(main.ToString());
+main.Invoke(instance, args);
+=> System.NullReferenceException: 
+    オブジェクト参照がオブジェクト インスタンスに設定されていません。
+対象オブジェクト mainが nullの様子。
+GetMethod()は publicのメソッドのみを抽出するので、
+internal Main()は抽出できず、nullとなる。
+
+そこで、GetMethod()引数、BindingFlags.NonPiblicを利用するも、
+引数 Binderが抽象クラスのため扱えず nullを代入。
+結果は上記と同様。
+
+【考察】2021-11-08
+[ReflectionSample.cs]で成功した Main()の呼出を応用。
+下記のように useClass.GetType() -> Stringとなり、
+Typeを動的に変数で指定できないことが原因で例外発生。
+useClass: CsharpBegin.A00_Template.MainExecute
+exeType: System.String
+
+ハンドルされていない例外: System.MissingMethodException:
+このプロジェクトで、引数なしコンストラクタ ーは定義されていません。
+ */
---
+++ Application/ProcessMainExe.cs
@@ -0,0 +1,89 @@
+/** 
+ *@title CsharpBegin / Application / ProcessMainExe.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@content ProcessMainExe
+ *         [VS] static Main()は１つのみなので、
+ *         各クラスの public Main()から実行するプログラム
+ * 
+ *@reference ■ C#のコマンド実行するには
+ *@url https://www.fenet.jp/dotnet/column/language/5722/
+ *@see Reference / ProcessStart.txt 
+    //Command Prompt呼出, コマンド入力, 実行
+    new ProcessStartInfo("cmd.exe", string command)
+        command: "/k dir" 
+           「/k」は実行後にコンソールを閉じないようにする
+           「/c」は実行後に消える？
+
+    Process.Start(ProcessStartInfo)
+     
+    // ウィンドウを表示しない
+    processStartInfo.CreateNoWindow = true;
+    processStartInfo.UseShellExecute = false; 
+ 
+     // 標準出力、標準エラー出力を取得できるようにする
+    processStartInfo.RedirectStandardOutput = true;
+    processStartInfo.RedirectStandardError = true; 
+ 
+     // コマンド実行
+    Process process = Process.Start(processStartInfo); 
+ 
+     // 標準出力・標準エラー出力・終了コードを取得する
+    string standardOutput = process.StandardOutput.ReadToEnd();
+    string standardError = process.StandardError.ReadToEnd();
+    int exitCode = process.ExitCode; 
+ 
+     process.Close(); 
+ 
+     // MessageBoxに標準出力を表示
+    MessageBox.Show(standardOutput);
+
+ *@author shika 
+ *@date 2021-11-18 
+*/
+using System; 
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CsharpBegin.Application 
+{ 
+    class ProcessMainExe 
+    { 
+        static void Main(string[] args) 
+        //public void Main(string[] args) 
+        {
+            //コマンドプロンプト呼出
+            string command = "/k csc /version";
+            ProcessStartInfo psInfo = new ProcessStartInfo("cmd.exe", command);
+            
+            //// ウィンドウを表示しない
+            psInfo.CreateNoWindow = true;
+            //psInfo.UseShellExecute = false;
+
+            // 標準出力、標準エラー出力を取得できるようにする
+            //psInfo.RedirectStandardOutput = true;
+            //psInfo.RedirectStandardError = true;
+            
+            //コマンド実行
+            Process process = Process.Start(psInfo);
+
+            //// 標準出力・標準エラー出力・終了コードを取得する
+            //string standardOutput = process.StandardOutput.ReadToEnd();
+            //string standardError = process.StandardError.ReadToEnd();
+            //int exitCode = process.ExitCode;
+
+            //process.Close();
+            ////MessageBox.Show(standardOutput);
+            //Console.WriteLine($"{standardOutput}");
+        }//Main() 
+    }//class 
+
+}
+
+/*
+>csc /version
+3.11.0-4.21403.6 (ae1fff34)
+*/
