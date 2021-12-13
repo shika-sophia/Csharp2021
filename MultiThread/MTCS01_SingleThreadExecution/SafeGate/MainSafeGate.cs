1a6109c9699abfcc21b75c2142741a50528e427f
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-12-13 12:56:01 +0900

    2021-12-13-1300

--- MultiThread/MTCS01_SingleThreadExecution/SafeGate/MainSafeGate.cs
+++ MultiThread/MTCS01_SingleThreadExecution/SafeGate/MainSafeGate.cs
@@ -4,25 +4,84 @@
  *@reference 結城 浩『デザインパターン入門 マルチスレッド編 [増補改訂版]』SB Creative, 2006 
  *@content MT 第１章 SingleThreadExecution / p50 /
  *         ～ 「この橋を渡れるのは、たった一人」
- *@subjecr MainSafeGate
- * 
+ *@subject SafeGate / List 1-4
+ *    ---- ONLY modification point from UnsafeGate ----
+ *@class MainSafeGate /◆Main() new UnsafeGate() -> new SafeGate()
+ *@class SafeGate
+ *         PassGate(){ lock(){ } }
+ *
+ *@see UnsafeGate / MainUnsafeGate.cs
+ *@see ../../ LockSample.cs
  *@author shika 
- *@date 2021-12-12 
+ *@date 2021-12-13 
 */
 using System; 
 using System.Collections.Generic; 
 using System.Linq; 
-using System.Text; 
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks; 
  
 namespace CsharpBegin.MultiThread.MTCS01_SingleThreadExecution.SafeGate 
 { 
     class MainSafeGate 
     {
-        //static void Main(string[] args)
-        public void Main(string[] args) 
+        static void Main(string[] args)
+        //public void Main(string[] args) 
         {
-            
+            Console.WriteLine("Testing Gate");
+            AbsMT01Gate gate = new SafeGate();
+            var psA = new PassengerThread(gate, "Alice", "Alaska");
+            var psB = new PassengerThread(gate, "Bobby", "Brazil");
+            var psC = new PassengerThread(gate, "Chris", "Canada");
+
+            Thread thA = new Thread(psA.Run);
+            Thread thB = new Thread(psB.Run);
+            Thread thC = new Thread(psC.Run);
+            thA.Start();
+            thB.Start();
+            thC.Start();
+            thA.Join();
+            thB.Join();
+            thC.Join();
+
+            Console.WriteLine();
+            Console.WriteLine(
+                $"Tested {PassengerThread.TEST_TIMES:###,###,###} times.");
+            Console.WriteLine(
+                $"{nameof(MainSafeGate)} Main(): END");
         }//Main() 
     }//class 
-} 
+}
+
+/*
+//==== Result as locked PassGate() ONLY ====
+【NOTE】SafeGate.cs
+ *  CheckGate() is called by locking PassGate(). 
+ *  ToString()  is called by locking CheckGate(). 
+ *  I need not lock CheckGate() and ToString().
+
+Testing Gate
+Alice BEGIN
+Bobby BEGIN
+Chris BEGIN
+Alice END
+Bobby END
+Chris END
+
+Tested 1,000,000 times.
+MainSafeGate Main(): END
+
+//==== Result as locked CheckGate() & ToString() ====
+【NOTE】These lock were mistakes. need lock PassGate().
+Testing Gate
+Alice BEGIN
+Bobby BEGIN
+Chris BEGIN
+**** BROKEN **** [206692] Alice, Alaska
+**** BROKEN **** [262868] Bobby, Brazil
+Alice END
+Bobby END
+Chris END
+MainSafeGate Main(): END
+ */
---
+++ MultiThread/MTCS01_SingleThreadExecution/SafeGate/SafeGate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.MultiThread.MTCS01_SingleThreadExecution.SafeGate
+{
+    class SafeGate : AbsMT01Gate
+    {
+        private string name = "Nobody";
+        private string address = "Nowhere";
+
+        public override void PassGate(string name, string address)
+        {
+            lock (this)
+            {
+                this.Count++;
+                this.name = name;
+                this.address = address;
+                CheckGate();
+            }
+        }//PassGate() as locked
+
+        internal override void CheckGate()
+        {
+            if (! name.StartsWith(address.Substring(0,1)))
+            {
+                Console.WriteLine($"**** BROKEN **** {this.ToString()}");
+            }
+        }//CheckGate() as locked
+
+        public override string ToString()
+        {
+            return $"[{Count}] {name}, {address}";
+        }//ToString() as locked
+    }//class
+}
