5db1f89221c05b4cbaefe98ec364cc07249b5183
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-12-14 14:18:19 +0900

    2021-12-14-1420

---
+++ MultiThread/MTCS01_SingleThreadExecution/Mutex/MainMutex.cs
@@ -0,0 +1,26 @@
+/** 
+ *@title CsharpBegin / MultiThread / MTCS01_SingleThreadExecution / Mutex / MainMutex.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@reference 結城 浩『デザインパターン入門 マルチスレッド編 [増補改訂版]』SB Creative, 2006 
+ *@content Mutex: Mutual Exclusion (= 相互排他)
+ * 
+ *@author shika 
+ *@date 2021-12-14 
+*/ 
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+ 
+namespace CsharpBegin.MultiThread.MTCS01_SingleThreadExecution.Mutex 
+{ 
+    class MainMutex 
+    { 
+        //static void Main(string[] args) 
+        public void Main(string[] args) 
+        {
+            
+        }//Main() 
+    } 
+} 
