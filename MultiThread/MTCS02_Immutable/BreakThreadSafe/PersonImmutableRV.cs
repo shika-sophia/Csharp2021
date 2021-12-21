88cb87fe9b8c027419287f86a5584d2aec6a9c61
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2021-12-21 19:04:47 +0900

    2021-12-21-1900

---
+++ MultiThread/MTCS02_Immutable/BreakThreadSafe/PersonImmutableRV.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.MultiThread.MTCS02_Immutable.BreakThreadSafe
+{
+    class PersonImmutableRV
+    {
+        private readonly string name;
+        private readonly string address;
+
+        public PersonImmutableRV(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public PersonImmutableRV(PersonMutable mutable)
+        {
+            lock (mutable)
+            {
+                this.name = mutable.GetName();
+                this.address = mutable.GetAddress();
+            }//lock
+        }
+
+        public PersonMutable GetMutable()
+        {
+            return new PersonMutable(this);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetAddress()
+        {
+            return address;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(PersonImmutableRV)}: Name {name} / Address {address}";
+        }
+    }//class
+}
---
+++ MultiThread/MTCS02_Immutable/BreakThreadSafe/PersonMutable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.MultiThread.MTCS02_Immutable.BreakThreadSafe
+{
+    sealed class PersonMutable
+    {
+        private string name;
+        private string address;
+
+        public PersonMutable(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public PersonMutable(PersonImmutableRV person) 
+            : this(person.GetName(), person.GetAddress()) { }
+
+        public void SetPerson(string name, string address)
+        {
+            lock (this)
+            {
+                this.name = name;
+                this.address = address;
+            }
+        }
+
+        public PersonImmutableRV GetImmutableRV()
+        {
+            lock (this)
+            {
+                return new PersonImmutableRV(this);
+            }
+        }
+
+        internal string GetName() //Called ONLY by PersonImmutableRV 
+        {
+            return name;
+        }
+
+        internal string GetAddress() //Called ONLY by PersonImmutableRV 
+        {
+            return address;
+        }
+
+        public override string ToString()
+        {
+            lock (this)
+            {
+                return $"{nameof(PersonMutable)}: Name {name} / Address {address}";
+            }
+        }
+    }//class
+}
