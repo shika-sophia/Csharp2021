646b4cd3a7e1e34de307b6be6774cba892e4e3b4
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-02-28 04:49:34 +0900

    2022-02-28-0500

--- MultiThread/MTCS08_WorkerThread/WorkerPool/ChannelPool.cs
+++ MultiThread/MTCS08_WorkerThread/WorkerPool/ChannelPool.cs
@@ -17,61 +17,70 @@ namespace CsharpBegin.MultiThread.MTCS08_WorkerThread.WorkerPool
 {
     class ChannelPool : AbsChannelMT08
     {
+        private const int REQ_LIMIT = 100;
         private readonly ConcurrentQueue<RequestMT08> queue
             = new ConcurrentQueue<RequestMT08>();
-        private const int REQ_LIMIT = 10;
         private List<WorkerThreadMT08> workerList;
         private int poolNum; //initial workerPool
-
 
         public ChannelPool(int poolNum = 2)
         {
             this.poolNum = poolNum;
-            workerList = new List<WorkerThreadMT08>(poolNum);            
-        }
+
+            workerList = new List<WorkerThreadMT08>();
+            for (int i = 0; i < poolNum; i++)
+            {
+                workerList.Add(
+                    new WorkerThreadMT08($"Worker-{i}", this));
+            }//for
+        }//constructor
 
         public override void StartWorker()
         {
-            for(int i = 0; i < poolNum; i++)
+            foreach(WorkerThreadMT08 worker in workerList)
             {
-                string thName = $"Worker-{i}";
-                workerList.Add(
-                    new WorkerThreadMT08(thName, this));             
-            }
-            
+                Thread workerTh = new Thread(worker.Run);
+                workerTh.Name = worker.thName;
+                workerTh.Start();
+            }//foreach
+        }//StartWorker()
+
+        private void AddWorker()
+        {
+            var workerNew = new WorkerThreadMT08(
+                    $"Worker-{(poolNum++) - 1}", this);
+            workerList.Add(workerNew);
+            Thread workerTh = new Thread(workerNew.Run);
+            workerTh.Name = workerNew.thName;
+            workerTh.Start();
         }
 
         public override void PutRequest(RequestMT08 req)
         {
             var sw = new Stopwatch();
             sw.Start();
-            while(queue.Count <= REQ_LIMIT)
+            while(queue.Count >= REQ_LIMIT)
             {
                 Thread.Sleep(100);
 
-                if(sw.ElapsedMilliseconds >= 10000)
+                if(sw.ElapsedMilliseconds >= 500)
                 {
-                    AdjustPool();
+                    AddWorker();
                     sw.Restart();
                 }
             }//while
-
+            sw.Stop();
+            
             queue.Enqueue(req);
             Thread.Yield();
         }//PutRequest()
-
-        private void AdjustPool()
-        {
-            workerList.Add(
-                new WorkerThreadMT08($"Worker-{++poolNum}", this));
-        }
 
         public override RequestMT08 TakeRequest()
         {
             RequestMT08 req;
             while (!queue.TryDequeue(out req))
             {
-                Thread.Sleep(Timeout.Infinite);
+                Thread.Sleep(100);
             }//while
 
             Thread.Yield();
---
+++ MultiThread/MTCS08_WorkerThread/WorkerPool/MainWorkerPool.cs
@@ -0,0 +1,134 @@
+/** 
+ *@title CsharpBegin / MultiThread / MTCS08_WorkerThread / WorkerPool / MainWorkerPool.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@reference 結城 浩『デザインパターン入門 マルチスレッド編 [増補改訂版]』SB Creative, 2006 
+ *@content 第８章 WorkerThread / 補講２ ThreadPool / p284
+ *         [Java] Executors.newFixThreadPool(int)
+ *         [C#]   ChannelPool 〔自己定義〕
+ *         ThreadPoolを持つクラスを自己定義し、
+ *         初期状態で WorkerThread 2を pool。
+ *         処理が停滞している場合は WorkerThreadを増やす。
+ *
+ *@based MainWorkerMT08
+ *       AbsChannelMT08 channel = new ChannelMT08(int thNum);
+ *       
+ *@class AbsChannelMT08 〔with Polymorphism〕
+ *         └ ChannelMT08
+ *         └ ChannelPool
+ *         
+ *@class MainWorkerPool
+ *       AbsChannelMT08 channel = new ChannelPool();
+ *       
+ *@class ChannelPool : AbsChannelMT08
+ *       / - const int REQ_LIMIT = 100;
+ *         - readonly ConcurrentQueue<RequestMT08> queue
+ *         - List<WorkerThreadMT08> workerList;
+ *         - int poolNum; //initial workerPool = 2 /
+ *         
+ *        + ChannelPool(int poolNum = 2) 
+ *          { new WorkerThreadMT08(string, AbsChannelMT08);
+ *            new List<WorkerThreadMT08>(); }
+ *        + void StrartWorker()
+ *        - void AddWorker()
+ *        + void        PutRequest(RequestMT08)
+ *        + RequestMT08 TakeRequest()
+ *
+ *@see MTCS05_ProducerComsumer / CakeTable / CakeTableBlocking.cs
+ *@see MTCS07_ThreadPerMessage / ThreadPoolSample / MainThreadPool.cs 
+ *@author shika 
+ *@date 2022-02-28 
+*/
+using CsharpBegin.MultiThread.MTCS08_WorkerThread.Worker;
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks; 
+ 
+namespace CsharpBegin.MultiThread.MTCS08_WorkerThread.WorkerPool 
+{ 
+    class MainWorkerPool 
+    { 
+        //static void Main(string[] args) 
+        public void Main(string[] args) 
+        {
+            //---- WorkerThread ----
+            AbsChannelMT08 channel = new ChannelPool();
+            channel.StartWorker();
+
+            //---- ClientThread ----
+            string[] clientAry = new string[]
+            {
+                "Alice", "Bobby", "Chris"
+            };
+
+            foreach (string thName in clientAry)
+            {
+                new Thread(
+                    new ClientThreadMT08(thName, channel).Run
+                ).Start();
+            }//foreach
+        }//Main() 
+    }//class 
+}
+
+/*
+//---- ClientThread / Thread.Sleep(random.Next(100)) ----
+//Requestが速すぎて、queueに貯まるので、Workerを増やしていく。
+
+Worker-1 Execute() [Request from Alice No.0]
+Worker-0 Execute() [Request from Bobby No.0]
+Worker-1 Execute() [Request from Chris No.0]
+Worker-0 Execute() [Request from Bobby No.1]
+Worker-0 Execute() [Request from Alice No.1]
+Worker-1 Execute() [Request from Chris No.1]
+Worker-1 Execute() [Request from Chris No.2]
+Worker-2 Execute() [Request from Alice No.2]
+Worker-3 Execute() [Request from Bobby No.2]
+Worker-0 Execute() [Request from Alice No.3]
+Worker-1 Execute() [Request from Bobby No.3]
+Worker-2 Execute() [Request from Chris No.3]
+Worker-1 Execute() [Request from Bobby No.4]
+Worker-3 Execute() [Request from Chris No.4]
+Worker-4 Execute() [Request from Alice No.4]
+  :
+
+//---- ClientThread / Thread.Sleep(random.Next(1000)) ----
+//Clientの待機時間 1000ミリ秒ランダムにすると、停滞なく、
+//初期の 2 WorkerThreadで処理可。
+Worker-0 Execute() [Request from Alice No.0]
+Worker-1 Execute() [Request from Bobby No.0]
+Worker-1 Execute() [Request from Chris No.0]
+Worker-0 Execute() [Request from Alice No.1]
+Worker-0 Execute() [Request from Bobby No.1]
+Worker-1 Execute() [Request from Chris No.1]
+Worker-0 Execute() [Request from Chris No.2]
+Worker-1 Execute() [Request from Alice No.2]
+Worker-0 Execute() [Request from Bobby No.2]
+Worker-1 Execute() [Request from Bobby No.3]
+Worker-0 Execute() [Request from Alice No.3]
+Worker-1 Execute() [Request from Chris No.3]
+Worker-0 Execute() [Request from Alice No.4]
+Worker-1 Execute() [Request from Bobby No.4]
+Worker-0 Execute() [Request from Chris No.4]
+Worker-0 Execute() [Request from Chris No.5]
+Worker-1 Execute() [Request from Alice No.5]
+Worker-0 Execute() [Request from Bobby No.5]
+Worker-1 Execute() [Request from Chris No.6]
+Worker-0 Execute() [Request from Alice No.6]
+Worker-1 Execute() [Request from Bobby No.6]
+Worker-0 Execute() [Request from Alice No.7]
+Worker-1 Execute() [Request from Chris No.7]
+Worker-0 Execute() [Request from Bobby No.7]
+Worker-1 Execute() [Request from Bobby No.8]
+Worker-1 Execute() [Request from Chris No.8]
+Worker-1 Execute() [Request from Alice No.8]
+Worker-1 Execute() [Request from Alice No.9]
+Worker-1 Execute() [Request from Bobby No.9]
+Worker-1 Execute() [Request from Chris No.9]
+Worker-1 Execute() [Request from Chris No.10]
+Worker-0 Execute() [Request from Bobby No.10]
+Worker-1 Execute() [Request from Alice No.10]
+  :
+ */
