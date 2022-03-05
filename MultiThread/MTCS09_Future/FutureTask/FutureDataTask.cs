e22d77619a07e5da5b7996201aaaa6acb36b5e1b
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-03-05 12:51:28 +0900

    2022-03-05-1245

---
+++ MultiThread/MTCS09_Future/FutureTask/FutureDataTask.cs
@@ -0,0 +1,17 @@
+﻿using CsharpBegin.MultiThread.MTCS09_Future.FutureSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBegin.MultiThread.MTCS09_Future.FutureTask
+{
+    class FutureDataTask : AbsDataMT09
+    {
+        public override string GetResult()
+        {
+            return "";
+        }
+    }//class
+}
---
+++ MultiThread/MTCS09_Future/FutureTask/MainFutureTask.cs
@@ -0,0 +1,30 @@
+/** 
+ *@title CsharpBegin / MultiThread / MTCS09_Future / FutureTask / MainFutureTask.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@reference 結城 浩『デザインパターン入門 マルチスレッド編 [増補改訂版]』SB Creative, 2006 
+ *@content 第９章 Future 補講 concurrent / p311 / List 9-6, 9-7
+ *         [Java] Callable<T>
+ *         [C#]   delegate void ThreadStart()
+ *
+ *@class MainFutureTask
+ * 
+ *@author shika 
+ *@date 2022-03-05 
+*/
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+ 
+namespace CsharpBegin.MultiThread.MTCS09_Future.FutureTask 
+{ 
+    class MainFutureTask 
+    { 
+        static void Main(string[] args) 
+        //public void Main(string[] args) 
+        { 
+            
+        }//Main() 
+    }//class 
+} 
