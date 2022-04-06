a2b85e465966fa68bc2d7462bdbd4d2d85151423
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-04-06 15:03:42 +0900

    Delete AbsIteratorGY01.cs

--- GofDesignYH/GY01_Iterator/AbsIteratorGY01.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CsharpBegin.GofDesignYH.GY01_Iterator
-{
-    public abstract class AbsIteratorGY01
-    {
-        public abstract bool HasNext();
-        public abstract object Next();
-    }//class
-}
