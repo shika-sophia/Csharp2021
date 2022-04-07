b16c85a131b5bb7c4d72b48441919c135996660f
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-04-07 11:56:21 +0900

    2022-04-07-1200

--- GofDesignYH/GY01_Iterator/IteratorSample/MainIteratorSample.cs
+++ GofDesignYH/GY01_Iterator/IteratorSample/MainIteratorSample.cs
@@ -3,7 +3,19 @@
  *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
  *@reference YG 結城 浩 『デザインパターン入門 Java言語 [増補改訂版]』SB Creative, 2004 
  *@content 第１章 Iterator / p2 / List 1-1 ～ 1-6
- *
+ *         || Design Pattern || 全体
+ *         ・具体的なクラスだけでコーディングすると、クラス間の結合が強くなり再利用が難しくなる
+ *           abstract(=抽象) - concreat(=具体) に分割することでクラス間の結合を緩和する。
+ *           => 部品としてクラスの再利用性を上げる
+ *           
+ *         || Iterator || ～ひとつひとつ数え上げる～
+ *         ・[Java] Iterator / [C#] Enumerator 言語仕様の標準ライブラリにも採用されたパターン
+ *         ・|| Composite || 再帰的な構造を持つプログラムには適用しずらい
+ *         ・Aggregate(集合体)と Iterator(数え上げ)のクラス群を分離
+ *           => Bookの配列/List機能の変更が Iteratorクラス群には影響を与えない。
+ *           => 互いに独立しているので、変更時には部分的な修正をすれば済む。
+ *              (全体を修正する必要がない)
+ *              
  *@class MainIteratorSample
  *       ◆Main()
  *       new BookShelf(int)
