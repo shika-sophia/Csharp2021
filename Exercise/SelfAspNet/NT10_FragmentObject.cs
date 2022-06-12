039de1a7c0e2d575c6f5253e65957c03e4981dd3
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-12 15:28:15 +0900

    2022-06-12-1530

---
+++ Exercise/SelfAspNet/NT10_FragmentObject.cs
@@ -0,0 +1,49 @@
+/** 
+ *@title CsharpBegin / Exercise / SelfAspNet / NT10_FragmentObject.cs 
+ *@reference 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017 
+ *@reference 山田祥寛『独習 ASP.NET 第６版』翔泳社, 2020 
+ *@content 第10章 FragmentObject / p491, p511
+ *
+ *@subject 練習問題 10-1, 10-2, 
+ * 
+ *@author shika 
+ *@date 2022-06-12 
+*/ 
+/*==== Appendix ==== 
+ *@date: 2022-06-12 (日) 
+ *@time: 15:15 ～ 15:18 (3分) 
+ *@rate: 60.00％ (○ 3 問 / 全 5 問) 
+*/ 
+namespace CsharpBegin.Exercise.SelfAspNet 
+{ 
+    class NT10_FragmentObject 
+    { 
+        static void Main(string[] args) 
+        //public void Main(string[] args) 
+        { 
+            new CsharpBegin.Exercise.ExerciseEditor(""); 
+        }//Main()  
+    }//class 
+}
+/* 
+2022-06-12 (日)
+==== Exercise Result ==== 
+◆〔1〕第10章 FragmentObject / 練習問題 10-1 
+○ (1) 別プロジェクト
+  =>〔SelfAspNet/SampleAsp/NT10_FlagmentObject/
+      UserControl/ChartCombinationWithSelctBrand.aspx〕
+
+◆〔2〕10-2 
+○ (1) 別プロジェクト
+  => 〔SelfAspNet/SampleAsp/NT10_FragmentObject/
+       MasterPageDiv/PravticeContent.aspx〕
+
+○ (2) Image 
+× (3) FindControl() => ○: Master.FindControl() 
+× (4) AlteringText => ○: AlternateText 
+*/
+/*==== Appendix ==== 
+ *@date: 2022-06-12 (日) 
+ *@time: 15:15 ～ 15:18 (3分) 
+ *@rate: 60.00％ (○ 3 問 / 全 5 問) 
+*/
